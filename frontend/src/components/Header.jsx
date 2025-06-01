33ce05b8d4b41de6ceb636dac040631008184cbe
iam-milad <miladnouri073@hotmail.com>
2025-06-01 18:46:09 +0100

    add cart functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
                   {
                     cartItems.length > 0 && (
                       <Badge pill bg="success" style={{marginLeft: "5px"}}>
-                        { cartItems.reduce((a, c) => a + c.qty, 0) }
+                        { cartItems.reduce((a, c) => a + Number(c.qty), 0) }
                       </Badge>
                     )
                   }
