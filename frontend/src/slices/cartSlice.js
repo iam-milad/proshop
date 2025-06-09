2e217facdabe002115e67c545a1d95f23a1692f5
iam-milad <miladnouri073@hotmail.com>
2025-06-09 21:07:16 +0100

    add payment page

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -31,7 +31,11 @@ const cartSlice = createSlice({
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
       return updateCart(state);
-    }
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
   },
 });
 
@@ -39,6 +43,7 @@ export const {
   addToCart,
   removeFromCart,
   saveShippingAddress,
+  savePaymentMethod
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
