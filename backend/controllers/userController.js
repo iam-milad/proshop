309c20d66cade3f122f726972962ffe5b0a4d25c
iam-milad <miladnouri073@hotmail.com>
2025-06-16 22:38:50 +0100

    fix login route bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc    Auth user & get token
-// @route   POST /api/users/auth
+// @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
